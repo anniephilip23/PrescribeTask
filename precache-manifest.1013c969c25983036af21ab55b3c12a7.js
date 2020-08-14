21a2186a99e22c1bdd9db1d23ed295c4de810cea
anniephilip23 <anniephilip23@gmail.com>
2020-08-15 03:47:41 +0530

    Updates

---
+++ precache-manifest.1013c969c25983036af21ab55b3c12a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383b5ba4737976317ebb253428c35876",
+    "url": "/PrescribeTask/index.html"
+  },
+  {
+    "revision": "ec068d2dfa29268374bb",
+    "url": "/PrescribeTask/static/css/main.fb773907.chunk.css"
+  },
+  {
+    "revision": "39472c38deb1628fac78",
+    "url": "/PrescribeTask/static/js/2.b0062624.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/PrescribeTask/static/js/2.b0062624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec068d2dfa29268374bb",
+    "url": "/PrescribeTask/static/js/main.d9ab7dce.chunk.js"
+  },
+  {
+    "revision": "e5b972f75872cda4c77a",
+    "url": "/PrescribeTask/static/js/runtime-main.993ad978.js"
+  }
+]);
